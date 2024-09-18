8b683cb9d236b516e0b7dd645554eb607235aab2
Diogo Mesquita <diogoluismesquita@outlook.com>
2024-09-18 16:09:11 +0100

    Remove HSTS

--- src/BudgetBase.Web.Razor/Program.cs
+++ src/BudgetBase.Web.Razor/Program.cs
@@ -58,11 +58,9 @@ namespace BudgetBase.Web.Razor
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseForwardedHeaders();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
